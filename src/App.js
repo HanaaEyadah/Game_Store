9c8d1ba5190abd81f6ce752c62e43df4593781e4
hanaa eyadah <hanaa.eyadah@outlook.com>
2021-02-16 00:00:20 +0300

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import img from "./game.jpeg"
+import ReactDOM from 'react-dom';
+import { icons } from "react-icons";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+
+   componentDidMount() {
+    document.title = 'Game_Store'
+  };
+      <head>Game_Store</head>
+      <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico"/>
+       <h1>Game_Store</h1>
+    <h4>In my store you can find whatever you want in gaming world </h4>
+    <img
+        alt="Game Store"
+        src="https://www.logolynx.com/images/logolynx/d3/d3287bc5c651f738e1df7f42ac9f97c0.jpeg"
+    ></img>
     </div>
+    
   );
+ 
+
+
 }
 
 export default App;
